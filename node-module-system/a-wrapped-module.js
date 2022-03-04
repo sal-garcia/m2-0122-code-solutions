034cee1b6e769a6da1719ce173cef59529f08562
sal garcia <salgarcia940@gmail.com>
2022-03-03 20:48:56 -0800

    node module system completed

---
+++ node-module-system/a-wrapped-module.js
@@ -0,0 +1,5 @@
+console.log('type of exports: ', typeof exports);
+console.log('type of require: ', typeof require);
+console.log('type of module: ', typeof module);
+console.log('type of __filename: ', typeof __filename);
+console.log('type of __dirname: ', typeof __dirname);
